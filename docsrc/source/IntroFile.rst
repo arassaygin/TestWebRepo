f82fd883cc0da1fcb5ca097312852575f9aac333
Arash Karshenass <arassaygin@metu.edu.tr>
2021-05-23 20:50:47 +0300

    thme updated
    theme changed to sphinx_rtd_theme

--- docsrc/source/IntroFile.rst
+++ docsrc/source/IntroFile.rst
@@ -9,3 +9,33 @@ This section introduces you with somthin important.
     :label: ideal gas
 
 According to ideal gas, Equation :math:numref:`ideal gas` we czan calculate absolute static pressure.
+
+.. code-block:: c++
+   :linenos:
+   :emphasize-lines: 5
+   :caption: the first code
+
+   class sample ;
+   for(int i=0;i<5;i++>) {
+       x[i] = i*i ;
+       if(X[i]>i)
+          break ;
+    }
+
+.. highlight:: c++
+
+The next patagraph is also a code::
+
+ string a = "the string" ;
+ char* b = "char series" ;
+ int i = 0 ;
+ double d = 2.5 ;
+
+For highlighiting code in **Cplusplus** format you can use either ``cpp`` or ``c++``.
+ 
+    
+ 
+
+
+
+
--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -24,7 +24,6 @@ author = 'Aras SAYGIN'
 # The full version, including alpha/beta/rc tags
 release = '2021'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -39,6 +38,7 @@ extensions = [
   'sphinx.ext.githubpages',
   'jupyter_sphinx',
   'nbsphinx',
+  'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,7 +55,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
